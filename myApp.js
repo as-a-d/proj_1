c18bf090e026bd32cf1377f94c24468bcae81cbe
as-a-d <asadmaqbool2006@gmail.com>
2025-06-08 17:12:43 +0000

    Upd 1

--- myApp.js
+++ myApp.js
@@ -2,10 +2,28 @@ const express = require('express')
 
 const app = express();
 
-app.get("/", function(req, res)  {
-  res.send("Hello World")
-})
+
+
+// date1 = new Date().toString()
+
+// console.log(date1)
+
+
 
 app.listen(5000, '0.0.0.0', () => {
   console.log('Server running on port 5000')
 })
+
+app.get("/", function(req, res)  {
+  res.send("Home page")
+  
+})
+
+app.get("/api/:date?", function(req, res) {
+  const date = new Date(req.params.date);
+  res.json({
+    unix: date.getDate(),
+    utc: date.toUTCString()
+
+  });
+});
